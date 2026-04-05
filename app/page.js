368c1cd954c43a4e86daf76fc8049bfc123de066
Arjanshr <sirarjan@gmail.com>
2026-04-05 15:27:07 +0545

    first push

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World — Test Frontend</h1>
+      <p>This will call your Laravel API next.</p>
+    </div>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // this enables static HTML export
+};
+
+module.exports = nextConfig;
