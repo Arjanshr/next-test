9009d9fbe104d107afd291356f0385bbfec45ed7
Arjanshr <sirarjan@gmail.com>
2026-04-05 16:15:21 +0545

    Test1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // this enables static HTML export
-};
+  reactStrictMode: true,
+  output: 'export', // ✅ enable static export
+}
 
 module.exports = nextConfig;
